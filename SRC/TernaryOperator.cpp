3c05de17bd7c7fbe1be4f1d71b6b0f2e5e71fcfb
Chessmaw <85143601+Chessmaw@users.noreply.github.com>
2023-07-28 01:20:45 -0600

    AddedChangesToTheTernary

--- SRC/TernaryOperator.cpp
+++ SRC/TernaryOperator.cpp
@@ -2,11 +2,20 @@
 
 int main()
 {
-    int grade = 45;
+    // Example
+    // Optinal (you can put it or not)
+    // int grade = 45;
+    //(grade >= 60) ? std::cout << "You pass!" : std::cout << "You fail!";
 
-    (grade >= 60) ? std::cout << "You pass!" : std::cout << "You fail!";
+    // int number = 9;
+    // number % 2 ? std::cout << "ODD" : std::cout << "Ok!";
+    // return 0;
 
-    return 0;
+    bool Hungry = false;
+    //(Hungry) ? std::cout << "I'm Hungry" : std::cout << "I'm not Hungry";
+    // or
+    std::cout<<((Hungry) ? "You are Hungry" : "You are full");
+    
 }
 /*
 

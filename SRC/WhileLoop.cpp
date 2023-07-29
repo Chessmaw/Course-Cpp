2abaf6f36946e64287a7b45ffb13660fece2fa55
Chessmaw <85143601+Chessmaw@users.noreply.github.com>
2023-07-28 21:57:13 -0600

    WhileLoop

---
+++ SRC/WhileLoop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::string name;
+    while (name.empty())
+    {
+        std::cout << "Enter your name:  ";
+        std::getline(std::cin, name);
+    }
+    std::cout << "Hello " << name;
+    return 0;
+}
